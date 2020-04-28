8202e3becdd51e13caa39fdadf72de8943f13dde
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-04-28 11:49:04 +1000

    added a sticky footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import TopBar from './components/TopBar';
 import SideBar from './components/SideBar';
 
+
 function App() {
   return (
-  	<div>
+  	<div className='container'>
   		<TopBar/>
   		<Header/>
   		<Nav/>
