1ecdcfb5f196d72cb8a6cf833f437aa61f22a8e9
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-04-28 13:44:04 +1000

    cleaned up directories

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import TopBar from './components/TopBar';
-import SideBar from './components/SideBar';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import TopBar from '../components/TopBar';
+import SideBar from '../components/SideBar';
 
 
 function App() {
